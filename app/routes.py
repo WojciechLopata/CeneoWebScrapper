4d26f47353943717b8ce52f2809e106ff766c528
WojciechLopata <s221547@student.uek.krakow.pl>
2022-05-31 00:30:33 +0200

    BOOTSTRAP WŁ

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,12 @@ from flask import render_template
 @app.route('/index/<name>')
 def index(name="<<hello world>>"):
     return render_template("index.html",text=name)
+@app.route("/autor")
+def autor():
+    return render_template("autor.html")
+@app.route("/extraction")
+def extraction():
+    return render_template("opinion_extr.html")
+@app.route("/lista")
+def lista():
+    return render_template("opinion_list.html")
