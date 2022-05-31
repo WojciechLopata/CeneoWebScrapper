a16f2a7c663779ba0a9a2ef9fb3d090b895a2a86
WojciechLopata <s221547@student.uek.krakow.pl>
2022-05-31 16:34:35 +0200

    2022-05-31 WŁ

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,63 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,url_for
+import requests
+import json
+from bs4 import BeautifulSoup
+def get_item(ancestor,selector,Attribute=None,return_list=False):
+    try:
+        if return_list:
+            pros=ancestor.select(selector)
+            return [item.get_text().strip() for item in pros]
+        if( Attribute):
+            return ancestor.select_one(selector)[Attribute]
+        return ancestor.select_one(selector).get_text().strip()
+    except (AttributeError,TypeError):
+        return None
+selectors={
+            "author":["span.user-post__author-name"],
+            "recomendation":["span.user-post__author-recomendation > em"],
+            "stars":["span.user-post__score-count"],
+            "content":["div.user-post__text"],
+            "usefull":["button.vote-yes > span"],
+            "useless":["button.vote-no > span"],
+            "published":["span.user-post__published > time:nth-child(1)","datetime"],
+            "purchased":["span.user-post__published > time:nth-child(2)","datetime"],
+            "pros":["div[class$=positives] ~ div.review-feature__item",None,True],
+            "cons":["div[class$=negatives] ~ div.review-feature__item",None,True]
 
+}
 @app.route('/')
-@app.route('/index/<name>')
-def index(name="<<hello world>>"):
-    return render_template("index.html",text=name)
+def index():
+    return render_template("index.html.jinja")
 @app.route("/autor")
 def autor():
-    return render_template("autor.html")
-@app.route("/extraction")
-def extraction():
-    return render_template("opinion_extr.html")
-@app.route("/lista")
-def lista():
-    return render_template("opinion_list.html")
+    return render_template("autor.html.jinja")
+@app.route("/extraction/<product_id>")
+def extraction(product_id):
+    url=f"https://www.ceneo.pl/{product_id}#tab=reviews"
+    all_opinions=[]
+    while(url):
+    
+        response= requests.get(url)
+        page = BeautifulSoup(response.text, 'html.parser')
+        opinions = page.select("div.js_product-review")
+        for opinion in opinions:
+            opinion_id=opinion["data-entry-id"]
+            single_opinion={
+            key:get_item(opinion,*value)
+                    for key,value in selectors.items() 
+        }
+            single_opinion["opinion_id"]=opinion["data-entry-id"]
+            all_opinions.append(single_opinion)
+        try:
+            url="https://www.ceneo.pl"+page.select_one("a.pagination__next")["href"]
+        except TypeError:
+            url=None
+
+    ##print(author,recomendation,opinion_id,stars,content,usefull,useless,published,purchased,sep="\n")
+    with open(f"app/opinions/{product_id}.json","w",encoding="UTF-8") as file:
+        json.dump(all_opinions,file,indent=4,ensure_ascii=False)
+    return redirect (url_for("extraction",product_id=product_id))
+@app.route("/products")
+def products():
+    pass
